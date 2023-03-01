c0bc7e62fea82d33aa68a3f2c0464f58119387fd
给曾经的自己 <564179234@qq.com>
2023-03-01 10:33:41 +0800

    Calculate

---
+++ src/interfaces/CalculatorServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 创建人 : Forpastime
+ * 创建时间: 2023/2/28 16:04
+ * 个人主页 : http://forpastime.com
+ */
+namespace interfaces;
+
+interface CalculatorServiceInterface
+{
+    public function add(int $a, int $b):int;
+}
